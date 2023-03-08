6f7b34d34b42f03c80bad1a920a9ebe190f0da03
namitamirge <dev.nmirge@gmail.com>
2023-03-08 14:55:11 -0500

    test1_2

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 # this script will create string,number and floating point number and print everything one by one
-mystring="hello"
-print(mystring)
-myfloat=10.0
-print(myfloat)
-myint=6
-print(myint)
+mystring=None
+myfloat=None
+myint=None
+#string=Hello
+if mystring=="hello":
+    print ("String: %s" % mystring)
