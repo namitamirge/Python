39995d0fa9f1c916b070f2da7decc63bde13ff30
namitamirge <dev.nmirge@gmail.com>
2023-03-08 16:59:41 -0500

    test1_3

--- test1.py
+++ test1.py
@@ -1,7 +1,18 @@
 # this script will create string,number and floating point number and print everything one by one
-mystring=None
-myfloat=None
-myint=None
+mystring="Hello"
+myfloat=10.0
+myint=4
 #string=Hello
-if mystring=="hello":
+if mystring=="Hello":
     print ("String: %s" % mystring)
+else :
+    print("Invalid string")
+if myfloat==10.0:
+    print ("Float value: %f" % myfloat)
+else :
+    ("Not correct float value")
+
+if myint==4:
+    print("Interger is %i:"%myint)
+else:
+    print("Invalid interger ")
