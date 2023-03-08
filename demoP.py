da922e2b07e74ae67d8e4c6221b1d78f1b265b15
namitamirge <dev.nmirge@gmail.com>
2023-03-07 21:32:57 -0500

    2

--- demoP.py
+++ demoP.py
@@ -1,2 +1,11 @@
+
+# this is comment
+# how to print multiple variable values at same time
 a, b, c,d = 3, 4, 5,7.0
 print(a, b,c,d)
+
+# mixing operator between numbers and string is not supported
+one=1
+two=2
+str="hello"
+print (int one + int two ,str str)
