5656c3d128f0babf94b623c0fc431341101436df
namitamirge <dev.nmirge@gmail.com>
2023-03-09 17:31:00 -0500

    test3_1

---
+++ test3.py
@@ -0,0 +1,15 @@
+#Auther- Namita
+# This script is for the displaying numbers in the numlist and text from txt and taking object from objectstring 
+#and printing individully
+
+numlist=[]
+txt=[]
+objstring=["pan","spoon","glass","pot","dish"]
+numlist.append(1)
+txt.append("Of")    # adding txt into txt list
+txt.append("water")
+print("Please bring %s "% numlist[0]) 
+print(objstring[2]) # taking third value from objstring
+
+print(txt[0])
+print(txt[1])
