7a35f2b8419b8812fd8c4bc2998088eaa991c67c
namitamirge <dev.nmirge@gmail.com>
2023-03-09 17:08:56 -0500

    test2_1

---
+++ test2.py
@@ -0,0 +1,15 @@
+#
+numlist=[]
+strlist=[]
+str=["cat", "dog", "mouse"]
+numlist.append(1)
+numlist.append(2)
+numlist.append(3)
+    ## code to display second name
+strlist.append("Hello")
+strlist.append("World")
+
+secname=str[1]
+print(numlist)
+print(strlist)
+print("Second name from the str is %s"%str[1])
