3498252f3befff8440f5c982cab8e5fecef1987d
namitamirge <dev.nmirge@gmail.com>
2023-03-09 22:05:41 -0500

    op_1

---
+++ op.py
@@ -0,0 +1,28 @@
+##Arithmatic operator
+number = 1 + 2 * 3 / 4.0
+print(number)
+num1=4
+num2=5
+sum=num1=num2
+
+a=4
+b=2
+sub=a-b
+print (sub)
+
+remainder = 6 % 4
+print(remainder)
+
+##using multiplication symbol make cube and square
+square=2 ** 2
+cube= 2 ** 3
+print(square)
+print(cube)
+
+#using operator for concatnate string
+text=("Hello"+""+"World")
+print(text)
+
+#multipling string to make repeating sequence
+str="Print two times" * 2
+print(str)
