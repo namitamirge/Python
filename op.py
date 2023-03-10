5ef23f133f2eadc4952e191f1004b4514a5a5f6a
namitamirge <dev.nmirge@gmail.com>
2023-03-09 22:12:10 -0500

    op_2

--- op.py
+++ op.py
@@ -26,3 +26,13 @@ print(text)
 #multipling string to make repeating sequence
 str="Print two times" * 2
 print(str)
+
+# adding two lists
+list1=[1,2,3,4]
+list2=[5,6,7,8]
+add=list1+list2
+print(add)
+#forming lsit with multiple sequence
+list11=[1,2,3,4]
+seq=list11 * 3
+print(seq)
