adb8a9eb3a220e9dee59bdb9f7f6f1a366fabd4e
namitamirge <dev.nmirge@gmail.com>
2023-03-14 22:31:02 -0400

    dice_1

---
+++ dice.py
@@ -0,0 +1,17 @@
+# importing the random module
+import random
+
+## this is the dice game
+#user will roll the dice
+#
+print('Hey, whats your name?')
+name=input()
+print('Please roll the dice ' +name)
+getnum=random.randint(0,6)
+# Program to generate a random number between 0 and 9
+#print(random.randint(0,6))
+print(getnum)
+
+#print("Do you want to roll the dice again?")
+
+print(random.randint(0,6))
