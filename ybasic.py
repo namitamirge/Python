81fd2fadcbc043f32283ef14f40efbaeddd6b2c0
namitamirge <dev.nmirge@gmail.com>
2023-03-27 11:10:13 -0400

    split str

--- ybasic.py
+++ ybasic.py
@@ -1,7 +1,23 @@
 #Ask for the name
-name=input("What is your name" , )
+
+name=input("What is your name?" )
+
+#remove whitespaces from str and capitalize user's name
+name=name.strip().title()
+
+#used to capitalise first letter of every word in string
+name=name.title()
+
+#Split first and last name from string
+first, last=name.split(" ")
+
+#Capitalize first word user name
+#name=name.capitalize()
+
+# print with ??? as seperator
 
 print("hello", name, sep="???")
 
-name=input("hello, \"friend\"") 
-name=input('hello, \"friend\"') 
+print("hello, \"friend\"")  #\used to keep "" in output
+print('hello, \"friend\"') 
+print(f"hello, {first}")
