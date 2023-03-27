bc59959de694e174f2422061792c5a3730458fa5
namitamirge <dev.nmirge@gmail.com>
2023-03-27 16:33:13 -0400

    mario loop

---
+++ mario.py
@@ -0,0 +1,33 @@
+#Mario
+
+#def main():
+ #   print_row(4)
+
+
+#def print_row(width):
+#    print("?" * width)
+
+#main()
+
+
+def main():
+    print_square(3)
+
+def print_square(size):
+
+    # for each row in square
+    for i in range(size):
+
+        #for each brick in row
+        for j in range(size):
+
+            #print brick
+            print("#", end="")
+
+        print()         #for printing new line after done with loop
+
+main()
+
+
+
+
