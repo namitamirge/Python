3e019dfcc846d998f4649a858c26db7c79c8d4e4
namitamirge <dev.nmirge@gmail.com>
2023-03-27 13:34:24 -0400

    grade.py

---
+++ condition.py
@@ -0,0 +1,12 @@
+##conditions
+
+x=input("What is value of x? :")
+y=input("What is value of y? :")
+
+if x < y or x > y:
+    print("x is not equal to y")
+
+
+else:
+    print("x is equal to y")
+
---
+++ grade.py
@@ -0,0 +1,16 @@
+# program to find out the grades of student
+
+score=int(input("Score is: "))
+
+if score >=90:
+    print("Grade A")
+elif score >=80:
+    print("grade B")
+
+elif score >=70:
+    print("grade C")
+    
+elif score >=50:
+    print("grade D")
+else:
+    print("Fail")
